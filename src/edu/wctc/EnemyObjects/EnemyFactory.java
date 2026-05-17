4fa86e48660693dd478168fb092ea0baa161d20f
Achaparas <achaparas@my.wctc.edu>
2026-05-17 03:54:53 -0500

    Quick Fix to EnemyFactory -I remembered how to have Cases have multiple values. Got rid of the massive list and made it look much better. -Still wish I could use ranges.

--- src/edu/wctc/EnemyObjects/EnemyFactory.java
+++ src/edu/wctc/EnemyObjects/EnemyFactory.java
@@ -12,28 +12,13 @@ public class EnemyFactory {
             //Enemy Names aren't unique, so don't need to be flexible
             //You *could* make them flexible so they could be unique, but I ain't gonna spend a day writing Top 100 Baby Names Edition 43
 
-            //Also. It's a bit annoying that I seemingly can't use Ranges for the switch statement conditionals.
-            //I spent a while looking into this, because I didn't want to just have a list of 10 possible states.
-            //I would've preferred something like what can be done in C#. I like C# better.
+            //I didn't remember how to have each case have multiple conditionals when I wrapped up the project.
+            //I just remembered how. Putting that in before going to bed
 
-            case 0:
-                return new WeakEnemy("Weak Enemy", attackTable, scoreTable);
-            case 1:
-                return new WeakEnemy("Weak Enemy", attackTable, scoreTable);
-            case 2:
-                return new WeakEnemy("Weak Enemy", attackTable, scoreTable);
-            case 3:
-                return new WeakEnemy("Weak Enemy", attackTable, scoreTable);
-            case 4:
+            case 0, 1, 2, 3, 4:
                 return new WeakEnemy("Weak Enemy", attackTable, scoreTable);
 
-            case 5:
-                return new BasicEnemy("Basic Enemy", attackTable, scoreTable);
-            case 6:
-                return new BasicEnemy("Basic Enemy", attackTable, scoreTable);
-            case 7:
-                return new BasicEnemy("Basic Enemy", attackTable, scoreTable);
-            case 8:
+            case 5, 6, 7, 8:
                 return new BasicEnemy("Basic Enemy", attackTable, scoreTable);
 
             case 9:
